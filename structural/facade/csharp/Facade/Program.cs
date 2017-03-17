8380b4eeb6824b4574420b8e1619e1d99a92db09
Phayao Boonon <phayaob@gmail.com>
2017-03-17 20:26:36 +0700

    Update implementation of facade pattern in C#

---
+++ structural/facade/csharp/Facade/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Facade
+{
+    public class Computer
+    {
+        public string GetElectricShock()
+        {
+            return "Ouch!";
+        }
+        public string MakeSound()
+        {
+            return "Beep beep!";
+        }
+        public string ShowLoadingScreen()
+        {
+            return "Loading...";
+        }
+        public string Bam()
+        {
+            return "Ready to be used!";
+        }
+        public string CloseEverything()
+        {
+            return "Bup bup bup buzzzz!";
+        }
+        public string Sooth()
+        {
+            return "Zzzzz";
+        }
+        public string PullCurrent()
+        {
+            return "Haaah!";
+        }
+    }
+
+    public class ComputerFacade
+    {
+        protected Computer computer;
+
+        public ComputerFacade(Computer computer)
+        {
+            this.computer = computer;
+        }
+
+        public void TurnOn() 
+        {
+            Console.WriteLine(this.computer.GetElectricShock());
+            Console.WriteLine(this.computer.MakeSound());
+            Console.WriteLine(this.computer.ShowLoadingScreen());
+            Console.WriteLine(this.computer.Bam());
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine(this.computer.CloseEverything());
+            Console.WriteLine(this.computer.PullCurrent());
+            Console.WriteLine(this.computer.Sooth());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ComputerFacade computer = new ComputerFacade(new Computer());
+            computer.TurnOn();
+            computer.TurnOff();
+        }
+    }
+}
---
+++ structural/facade/csharp/FacadeUnitTest/UnitTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FacadeUnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestCase()
+        {
+            // Expected result            
+
+            // Actual result
+
+            // Test
+
+        }
+    }
+}
