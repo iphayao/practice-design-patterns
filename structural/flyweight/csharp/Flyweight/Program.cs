c57872665935cd314d6262043b3a44c3e933a634
Phayao Boonon <phayaob@gmail.com>
2017-03-19 21:39:19 +0700

    Update implementation of flyweight pattern in C#

---
+++ structural/flyweight/csharp/Flyweight/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Flyweight
+{
+    class KarakTea
+    {
+
+    }
+    class TeaMaker
+    {
+        protected Dictionary<string, KarakTea> availableTea = new Dictionary<string, KarakTea>();
+        public KarakTea make(string preference)
+        {
+            if(!this.availableTea.ContainsKey(preference))
+            {
+                this.availableTea[preference] = new KarakTea();
+            }
+            return this.availableTea[preference];
+        }
+    }
+
+    class TeaShop
+    {
+        protected Dictionary<int, KarakTea> orders = new Dictionary<int, KarakTea>();
+        protected TeaMaker teaMaker;
+
+        public TeaShop(TeaMaker teaMaker)
+        {
+            this.teaMaker = teaMaker;
+        }
+
+        public void TakeOrder(string teaType, int table) 
+        {
+            this.orders[table] = this.teaMaker.make(teaType);
+        }
+        public void serve()
+        {
+            foreach(KeyValuePair<int, KarakTea> tea in this.orders)
+            {
+                Console.WriteLine("Serving data to table# " + tea.Key.ToString());
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeaMaker teaMaker = new TeaMaker();
+            TeaShop shop = new TeaShop(teaMaker);
+
+            shop.TakeOrder("less sugar", 1);
+            shop.TakeOrder("more milk", 2);
+            shop.TakeOrder("without sugar", 5);
+
+            shop.serve();
+        }
+    }
+}
