5ee80c39da8202a5794154ad82d90418c2021891
Phayao Boonon <phayaob@gmail.com>
2017-03-21 21:03:09 +0700

    Update implementaton of mediator pattern in C#

---
+++ behavioral/mediator/csharp/Mediator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Mediator
+{
+    interface IChatRoomMediator
+    {
+        void ShowMessage(User user, string message);
+    }
+
+    class ChatRoom : IChatRoomMediator
+    {
+        public void ShowMessage(User user, string message)
+        {
+            string sender = user.GetName();
+            Console.WriteLine("{0} [{1}]:{2}", DateTime.Now.ToString("MMMM dd, yyyy H:mm:ss"), sender, message);
+        }
+    }
+
+    class User
+    {
+        protected string name;
+        protected IChatRoomMediator chateMediator;
+
+        public User(string name, IChatRoomMediator chateMediator)
+        {
+            this.name = name;
+            this.chateMediator = chateMediator;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public void send(string message)
+        {
+            this.chateMediator.ShowMessage(this, message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatRoom mediator = new ChatRoom();
+
+            User john = new User("John Doe", mediator);
+            User jane = new User("Jane Doe", mediator);
+
+            john.send("Hi there!");
+            jane.send("Hey!");
+        }
+    }
+}
