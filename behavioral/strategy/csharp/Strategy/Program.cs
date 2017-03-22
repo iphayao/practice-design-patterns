b785a88e23100de80f55faea0bab6003700ba0fe
Phayao Boonon <phayaob@gmail.com>
2017-03-22 22:47:10 +0700

    Update implementation of stretegy patter in C#

---
+++ behavioral/strategy/csharp/Strategy/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Strategy
+{
+    interface ISortStrategy
+    {
+        int[] Sort(int[] dataset);
+    }
+
+    class BubbleSortStrategy : ISortStrategy
+    {
+        public int[] Sort(int[] dataset)
+        {
+            Console.WriteLine("Sorting using buble sort");
+            return dataset;
+        }
+    }
+
+    class QuickSortStrategy : ISortStrategy
+    {
+        public int[] Sort(int[] dataset)
+        {
+            Console.WriteLine("Sorting using quick sort");
+            return dataset;
+        }
+    }
+
+    class Sorter
+    {
+        protected ISortStrategy sorter;
+        public Sorter(ISortStrategy sorter)
+        {
+            this.sorter = sorter;
+        }
+        public int[] Sort(int[] dataset)
+        {
+            return this.sorter.Sort(dataset);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dataset = {1, 5, 4, 3, 2, 8};
+
+            Sorter sorter;
+            sorter = new Sorter(new BubbleSortStrategy());
+            sorter.Sort(dataset);
+
+            sorter = new Sorter(new QuickSortStrategy());
+            sorter.Sort(dataset);
+
+        }
+    }
+}
